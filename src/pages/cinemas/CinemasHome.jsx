9e30690019791f06b79666d70f17905f0fac66dc
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-18 00:58:58 -0500

    Update page CinemasHome useDispatch fetchReadPremieres

--- src/pages/cinemas/CinemasHome.jsx
+++ src/pages/cinemas/CinemasHome.jsx
@@ -1,9 +1,14 @@
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchReadPremieres } from "../../redux/slices/cinemas.slices";
 
 const CinemasHome = () => {
+  const dispatch = useDispatch();
 
   useEffect(() => {
     document.title = '¡Bienvenido a Cineverse Perú!';
+    dispatch(fetchReadPremieres());
+    // eslint-disable-next-line
   }, []);
 
   return (<h1>Home</h1>);
