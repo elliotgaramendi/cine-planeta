693b6c9016d0d18759c792a35ac09c0a0314efe4
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-17 22:26:29 -0500

    layout CinemasLayout inicial

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import CinemasLayout from './layouts/CinemasLayout';
 
 function App() {
   return (
     <Routes>
-      <Route path='/'>
+      <Route path='/' element={<CinemasLayout />}>
         <Route path='dulceria' element={<h1>Dulcería</h1>} />
         <Route index element={<h1>Home</h1>} />
         <Route path='*' element={<h1>404</h1>} />
---
+++ src/layouts/CinemasLayout.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom";
+
+const CinemasLayout = () => {
+  const applicationName = "🌌cineverse🌌";
+  const credits = {
+    year: new Date().getFullYear(),
+    author: 'ELGS'
+  };
+
+  return (
+    <>
+      <header>{applicationName}</header>
+      <main className="main">
+        <Outlet />
+      </main>
+      <footer className="footer">
+        🦄Copyright &copy; {credits.year} <a href="https://www.linkedin.com/in/elliotgaramendi/" target="_blank" rel="noopener noreferrer">{credits.author}.</a> Todos los derechos reservados.🦄
+      </footer>
+    </>
+  );
+};
+
+export default CinemasLayout;
