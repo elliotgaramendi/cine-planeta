d238ecfbe41469ed2abc3a899ada0da61431c055
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-17 23:54:07 -0500

    Update src App.js redux

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import {
 } from 'react-router-dom';
 import CinemasLayout from './layouts/CinemasLayout';
 import CinemasHome from './pages/cinemas/CinemasHome';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<CinemasLayout />}>
-        <Route path='dulceria' element={<h1>Dulcería</h1>} />
-        <Route index element={<CinemasHome />} />
-        <Route path='*' element={<h1>404</h1>} />
-      </Route>
-      <Route path='/autenticacion/login' element={<h1>Login</h1>} />
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path='/' element={<CinemasLayout />}>
+          <Route path='dulceria' element={<h1>Dulcería</h1>} />
+          <Route index element={<CinemasHome />} />
+          <Route path='*' element={<h1>404</h1>} />
+        </Route>
+        <Route path='/autenticacion/login' element={<h1>Login</h1>} />
+      </Routes>
+    </Provider>
   );
 };
 
