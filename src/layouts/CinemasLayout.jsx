5a3eb18b97d1ddac1fec66128800909a953ebe41
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-17 22:54:53 -0500

    Update public index.html & layout CinemasLayout

--- src/layouts/CinemasLayout.jsx
+++ src/layouts/CinemasLayout.jsx
@@ -9,11 +9,11 @@ const CinemasLayout = () => {
 
   return (
     <>
-      <header>{applicationName}</header>
-      <main>
+      <header className="header">{applicationName}</header>
+      <main className="main">
         <Outlet />
       </main>
-      <footer>
+      <footer className="footer">
         🦄Copyright &copy; {credits.year} <a href="https://www.linkedin.com/in/elliotgaramendi/" target="_blank" rel="noopener noreferrer">{credits.author}.</a> Todos los derechos reservados.🦄
       </footer>
     </>
