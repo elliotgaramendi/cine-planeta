e055da5b486507b49215b1fb708a85a8bf207302
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-19 01:51:10 -0500

    Update component CinemasSlide

--- src/components/cinemas/CinemasSlide.jsx
+++ src/components/cinemas/CinemasSlide.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom";
+
 const CinemasSlide = ({ premiere }) => {
   const { description, image } = premiere;
   return (
-    <figure className="slick__slide-figure">
-      <img src={image} alt={description} className="slick__slide-image" />
-    </figure>
+    <div className="slick-slider__slide-container">
+      <img src={image} alt={description} className="slick-slider__slide-image" />
+      <div className="slick-slider__content">
+        <h2 className="slick-slider__content-title">
+          {description}
+        </h2>
+        <div className="slick-slider__content-details">
+          Cuando sientas que no puedes continuar, cuando sientas que los sueños se te van, cuando creas que ya no hay más razón para avanzar, mantener la fe y piensa en la razón por la cual te esforzaste hasta hoy y vuelve a intentar.
+        </div>
+        <Link to="/autenticacion/login" className="slick-slider__content-buy">Comprar</Link>
+      </div>
+    </div>
   );
 };
 
