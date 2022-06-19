ab7801b2d22fb0ee2440fcdb551ce2ceafdc6a02
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-18 22:20:42 -0500

    Update styles CinemasHome.scss

--- src/components/cinemas/CinemasCard.jsx
+++ src/components/cinemas/CinemasCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const CinemasCard = ({ premiere }) => {
   const { description, image } = premiere;
   return (
-    <div className="home-gallery__card-container">
+    <div className="home-gallery__card-container home-gallery__card-container--premiere">
       <img src={image} alt={description} className="home-gallery__card-image" loading="lazy" />
       <div className="home-gallery__card-extra">
         <Link to="/autenticacion/login" className="home-gallery__card-buy">Comprar</Link>
