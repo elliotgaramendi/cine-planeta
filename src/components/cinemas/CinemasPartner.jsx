aaf22777df5fd3708c73096afb2944d337658da8
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-20 16:14:00 -0500

    Update component CinemasPartner

--- src/components/cinemas/CinemasPartner.jsx
+++ src/components/cinemas/CinemasPartner.jsx
@@ -6,9 +6,9 @@ const CinemasPartner = () => {
     <section className="home-partner">
       <div className="home-partner__container">
         <div className="home-partner__card">
-          <h3 className="home-partner__card-title">Bienvenido socio Cineverse</h3>
+          <h3 className="home-partner__card-title">Bienvenido Socio Cineverse</h3>
           <img src="https://cdn.pixabay.com/photo/2018/08/31/02/28/credit-3643710_960_720.png" alt="membership card" className="home-partner__card-image" />
-          <p className="home-partner__card-message">¿Estás preparado para vivir la mejor experiencia y gozar por los mejores beneficios? socio Cineverse es lo que tanto deseabas.</p>
+          <p className="home-partner__card-message">¿Estás preparado para vivir la mejor experiencia y gozar por los mejores beneficios? Socio Cineverse es lo que tanto deseabas.</p>
           <div className="home-partner__card-button-container">
             <Link to={`/autenticacion/login`} className="home-partner__card-login-button home-partner__card-login-button--outline">
               Inicia
