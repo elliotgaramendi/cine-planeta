18c7e54fd61755ba897d3f0a1e9706bdaf11d890
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-20 18:16:39 -0500

    Update component Footer

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import '../../styles/scss/Footer.scss';
 
 const Footer = ({ credits }) => {
-  const { author, currentYear } = credits;
+  const { author, year } = credits;
 
   return (
     <footer className="footer">
@@ -98,7 +98,7 @@ const Footer = ({ credits }) => {
       <nav className="footer-nav-footer">
         <div className="footer-nav-footer__container">
           <Link to="/" className="footer-nav-footer__subtitle">Condiciones de uso y seguridad</Link>
-          <h5 className="footer-nav-footer__subtitle">Copyright &copy; {currentYear} <a href="https://www.linkedin.com/in/elliotgaramendi/" className="footer-nav-footer__subtitle-author" target="_blank" rel="noopener noreferrer">{author}</a></h5>
+          <h5 className="footer-nav-footer__subtitle">Copyright &copy; {year} <a href="https://www.linkedin.com/in/elliotgaramendi/" className="footer-nav-footer__subtitle-author" target="_blank" rel="noopener noreferrer">{author}</a></h5>
           <Link to="/" className="footer-nav-footer__subtitle footer-nav-footer__subtitle--icon"><span className="footer-nav-footer__subtitle-icon"><i className="bi bi-book"></i></span> Libro de reclamaciones</Link>
         </div>
       </nav>
