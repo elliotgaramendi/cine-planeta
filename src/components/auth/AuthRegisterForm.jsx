3ddd4d7002ca17beb6926e9fa0a208f427593b59
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-23 15:59:04 -0500

    Update redux auth.slices fetchAuthRegister

--- src/components/auth/AuthRegisterForm.jsx
+++ src/components/auth/AuthRegisterForm.jsx
@@ -1,7 +1,13 @@
 import * as yup from 'yup';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { useDispatch } from 'react-redux';
+import { fetchAuthRegister } from '../../redux/slices/auth.slices';
+import { useNavigate } from 'react-router-dom';
 
 const AuthRegisterForm = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   return (
     <Formik
       initialValues={{
@@ -12,10 +18,11 @@ const AuthRegisterForm = () => {
         email: yup.string().email().required('El correo es requerido'),
         password: yup.string().required('La contraseña es requerido').min(6)
       })}
-      onSubmit={(values, actions) => {
-        console.log(values);
+      onSubmit={async (values, actions) => {
+        await dispatch(fetchAuthRegister(values));
         actions.setSubmitting(false);
-        // actions.resetForm();
+        actions.resetForm();
+        navigate('/dulceria');
       }}
     >
       {({ handleSubmit, isSubmitting }) => {
@@ -38,7 +45,7 @@ const AuthRegisterForm = () => {
                 className="auth__button-secondary"
                 disabled={isSubmitting}
               >
-                {isSubmitting ? <i className="bi bi-arrow-repeat"></i> : <><i className="bi bi-person-circle"></i> Unirme</>}
+                {isSubmitting ? <div className="animate-spin"><i className="bi bi-arrow-repeat"></i></div> : <><i className="bi bi-person-circle"></i> Unirme</>}
               </button>
             </div>
           </Form>
--- src/redux/slices/auth.slices.js
+++ src/redux/slices/auth.slices.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../configs/authFirebase";
 
 const initialState = {
   loading: false,
@@ -6,10 +8,52 @@ const initialState = {
   user: {}
 };
 
-const cinemasSlices = createSlice({
+const authSlices = createSlice({
   name: 'auth',
   initialState,
-  reducers: {}
+  reducers: {
+    fetchAuthRegisterRequest(state, action) {
+      return {
+        ...state,
+        loading: action.payload
+      };
+    },
+    fetchAuthRegisterSuccess(state, action) {
+      return {
+        ...state,
+        loading: false,
+        error: {},
+        user: action.payload
+      };
+    },
+    fetchAuthRegisterError(state, action) {
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+        user: {}
+      };
+    }
+  }
 });
 
-export default cinemasSlices.reducer;
+const {
+  fetchAuthRegisterError,
+  fetchAuthRegisterRequest,
+  fetchAuthRegisterSuccess
+} = authSlices.actions;
+
+export const fetchAuthRegister = (user) => {
+  return (async (dispatch) => {
+    dispatch(fetchAuthRegisterRequest(true));
+    try {
+      const { email, password } = user;
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      dispatch(fetchAuthRegisterSuccess(userCredential.user.reloadUserInfo));
+    } catch (error) {
+      dispatch(fetchAuthRegisterError(error));
+    }
+  });
+};
+
+export default authSlices.reducer;
