ede60696d6afb06ce30c8bf0f196c9a2d8af43f4
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-23 12:30:14 -0500

    Update redux store authSlices

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlices from "./slices/auth.slices";
 import cinemasSlices from "./slices/cinemas.slices";
 
 const store = configureStore({
   reducer: {
-    cinemasStore: cinemasSlices
+    cinemasStore: cinemasSlices,
+    authStore: authSlices
   }
 });
 
