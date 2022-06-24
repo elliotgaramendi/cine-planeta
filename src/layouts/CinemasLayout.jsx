9dff94538bc4db9f6345967efd2ea853f8251810
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-24 01:50:26 -0500

    Update CinemasLayout

--- src/layouts/CinemasLayout.jsx
+++ src/layouts/CinemasLayout.jsx
@@ -1,23 +1,9 @@
-import { useDispatch } from 'react-redux';
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/sections/Footer";
 import Header from "../components/sections/Header";
-import { auth } from "../configs/authFirebase";
-import { fetchAuthLoginSuccess } from "../redux/slices/auth.slices";
 import { applicationName } from "../utils/generalInformation";
 
 const CinemasLayout = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (currentUser) => {
-      dispatch(fetchAuthLoginSuccess(currentUser.reloadUserInfo));
-    });
-    // eslint-disable-next-line
-  }, []);
-
   return (
     <>
       <Header
